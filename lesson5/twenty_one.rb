4386d1c743fe61e569bb31b47ae2563a5cb584b3
Matt Borgman <mgborgman@gmail.com>
2017-08-05 15:16:01 -0700

    game is fully functional, next step is refactoring and gameplay improvements

--- lesson5/twenty_one.rb
+++ lesson5/twenty_one.rb
@@ -1,19 +1,34 @@
 require 'pry'
 
-class Participant
-  attr_accessor :hand
+class Participant 
+  attr_accessor :hand, :total
   def initialize
     @hand = []
+    @total = calculate_total
   end
 
-  def hit
-    puts "this is a hit"
+  def total
+    @total = calculate_total
   end
+
+  # def hit
+
+  # end
 
   def stay
+    true
   end
 
   def busted?
+    #total = calculate_total
+    total > 21
+  end
+
+  def show_hand
+    #binding.pry
+    hand.each do |card|
+      "#{card}"
+    end
   end
 
   def values(value)
@@ -43,7 +58,7 @@ class Participant
     when 'K'
       10
     when 'A'
-      if total + 11 <= 21
+      if self.total <= 21
         11
       else
         1
@@ -51,30 +66,46 @@ class Participant
     end
   end
 
-  def total
+  def calculate_total
     total = 0
     index = 0
-    while index <= hand.length
-      temp = values(hand[index].value)
-      total += temp
+    while index < hand.length
+      if hand[index].value == 'A'
+        if total <= 10
+          value = 11
+        else
+          value = 1
+        end
+      else
+        value = values(hand[index].value)
+      end
+      total += value
       index += 1
     end
-    total
+    self.total = total
   end
 end
 
 class Player < Participant
-  def check_hand
+  def show_hand
     puts "Your Hand:"
-    "#{hand[0]} #{hand[1]}"
+    super
     puts "Your total is #{total}"
   end
 end
 
 class Dealer < Participant
+  attr_reader :deck
+
+  def initialize
+    @deck = Deck.new
+    super
+  end
+
   def show_hand
     puts "Dealers Hand:"
-    "#{hand[0]}"
+    super
+    puts "Dealers total is #{total}"
   end
 end
 
@@ -119,13 +150,15 @@ class Card
   end
 
   def to_s
-    puts "+-----+"
-    puts "|#{@value}    |"
-    puts "|     |"
-    puts "|  #{@suit}  |"
-    puts "|     |"
-    puts "|    #{@value}|"
-    puts "+-----+"
+    print """
+    +-----+
+    |#{@value}    |
+    |     |
+    |  #{@suit}  |
+    |     |
+    |    #{@value}|
+    +-----+
+    """
   end
 end
 
@@ -134,16 +167,91 @@ class Game
   def initialize
     @player = Player.new
     @dealer = Dealer.new
-    @deck = Deck.new
+    #@deck = Deck.new
   end
 
   def start
-    deck.deal_hand(player, dealer)
-    player.check_hand
+    dealer.deck.deal_hand(player, dealer)
     dealer.show_hand
-    # player_turn
-    # dealer_turn
-    # show_result
+    player.show_hand
+    player_turn
+    dealer_turn
+    dealer.show_hand
+    display_results
+  end
+
+  def prompt_for_move
+    move = nil
+    loop do
+      puts "would you like to (H)it or (S)tay?"
+      move = gets.chomp.strip.downcase
+      break if %w[h s].include?(move)
+      puts "Please enter 'h' or 's'."
+    end
+    move
+  end
+
+  def perform_move_player
+    loop do 
+      move = prompt_for_move
+      case move
+      when 'h'
+        dealer.deck.deal_card(player)
+        player.show_hand
+      when 's'
+        player.stay
+      end
+      break if move == 's' || player.busted?
+    end
+  end
+
+  def perform_move_dealer
+    dealer.deck.deal_card(dealer)
+  end
+
+  def player_turn
+    loop do
+      break if player.busted?
+      perform_move_player
+      break if player.stay
+    end
+  end
+
+  def dealer_turn
+    loop do
+      break if dealer.total == 21
+      break if dealer.total >= 17
+      perform_move_dealer
+    end
+  end
+
+  def determine_winner
+    if player.busted? && dealer.busted?
+      false
+    elsif player.busted?
+      dealer
+    elsif dealer.busted?
+      player
+    elsif player.total == dealer.total
+      false
+    elsif player.total > dealer.total
+      player
+    elsif dealer.total > player.total
+      dealer
+    end
+  end
+
+  def display_results
+    winner = determine_winner
+    case winner
+    when player
+      puts "You Won!"
+    when dealer
+      puts "The House Won."
+    when false
+      puts "It's a Tie."
+    end
+    puts "Your total: #{player.total}  Dealer Total: #{dealer.total}"
   end
 end
 
