68a6c196d993864c92805c921f3a1c7ff095fb29
Matt Borgman <mgborgman@gmail.com>
2017-08-05 17:13:06 -0700

    added play_again? method, and improved UI

--- lesson5/twenty_one.rb
+++ lesson5/twenty_one.rb
@@ -1,34 +1,26 @@
 require 'pry'
 
-class Participant 
+class Participant
   attr_accessor :hand, :total
   def initialize
     @hand = []
-    @total = calculate_total
+    @total = 0
   end
 
   def total
     @total = calculate_total
   end
 
-  # def hit
-
-  # end
-
   def stay
     true
   end
 
   def busted?
-    #total = calculate_total
     total > 21
   end
 
   def show_hand
-    #binding.pry
-    hand.each do |card|
-      "#{card}"
-    end
+    hand.each(&:to_s)
   end
 
   def values(value)
@@ -58,7 +50,7 @@ class Participant
     when 'K'
       10
     when 'A'
-      if self.total <= 21
+      if total <= 21
         11
       else
         1
@@ -95,11 +87,27 @@ class Player < Participant
 end
 
 class Dealer < Participant
+  UNKOWN_CARD = """
+    +-----+
+    |?    |
+    |     |
+    |  ?  |
+    |     |
+    |    ?|
+    +-----+
+    """
+
   attr_reader :deck
 
   def initialize
     @deck = Deck.new
     super
+  end
+
+  def show_hand_initial
+    puts "Dealers Hand:"
+    hand[0].to_s
+    puts UNKOWN_CARD
   end
 
   def show_hand
@@ -118,11 +126,7 @@ class Deck
   def initialize
     @cards = []
     @test = 'Hello'
-    SUITS.each do |suit|
-      VALUES.each do |value|
-        @cards << Card.new(suit, value)
-      end
-    end
+    reset_deck
   end
 
   def shuffle
@@ -139,6 +143,14 @@ class Deck
 
   def deal_card(player)
     player.hand << cards.shift
+  end
+
+  def reset_deck
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        @cards << Card.new(suit, value)
+      end
+    end
   end
 end
 
@@ -165,19 +177,54 @@ end
 class Game
   attr_reader :player, :dealer, :deck
   def initialize
-    @player = Player.new
-    @dealer = Dealer.new
-    #@deck = Deck.new
+    reset
   end
 
   def start
-    dealer.deck.deal_hand(player, dealer)
-    dealer.show_hand
-    player.show_hand
-    player_turn
-    dealer_turn
-    dealer.show_hand
-    display_results
+    display_welcome_message
+    loop do
+      dealer.deck.deal_hand(player, dealer)
+      dealer.show_hand_initial
+      player.show_hand
+      player_turn
+      wait
+      dealer_turn
+      dealer.show_hand
+      wait
+      display_results
+      break unless play_again?
+      reset
+    end
+    display_goodbye_message
+  end
+
+  def display_welcome_message
+    puts "Welcome to Twenty One!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Twenty One!"
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "would you like to play again? (y/n)"
+      answer = gets.chomp.strip.downcase
+      break if %w[y n].include?(answer)
+      puts "Please answer y or n"
+    end
+    answer == 'y'
+  end
+
+  def wait
+    system 'sleep 1'
+  end
+
+  def reset
+    system 'clear'
+    @player = Player.new
+    @dealer = Dealer.new
   end
 
   def prompt_for_move
@@ -186,13 +233,13 @@ class Game
       puts "would you like to (H)it or (S)tay?"
       move = gets.chomp.strip.downcase
       break if %w[h s].include?(move)
-      puts "Please enter 'h' or 's'."
+      puts "Please enter h or s."
     end
     move
   end
 
   def perform_move_player
-    loop do 
+    loop do
       move = prompt_for_move
       case move
       when 'h'
@@ -257,4 +304,3 @@ end
 
 game = Game.new
 game.start
-
